9918e26083ec3fb0f2b44b93d00aca208b8c5ebb
renzon <renzon@gmail.com>
2016-09-30 22:13:44 -0300

    First simple implementation

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ You need generate some CLI commands and start simple as
 ...    return 'git'
 >>> git()
 'git'
+
 ```
 But you realize it has many options:`
 
@@ -29,23 +30,100 @@ But you realize it has many options:`
 'git -v'
 >>> git(p=True, v=True)
 'git -p -v'
+
 ```
 So the spaghetti code begins.
 
 # Lib Goal
 
+The lib goal is provide a fluent interface to generate cli commands:
+
 ```python
->>> from pytocli.example.git.Git
+>>> from tests.test_pytocli import Git
 >>> str(Git())
 'git'
->>> str(Git().p())
-'git'
->>> str(Git().v())
-'git -v'
->>> str(Git().p().v())
-'git -p -v'
->>> help(Git.p)
-'insert -p option which paginates result'
->>> help(Git.p)
-'insert -v to verbose mode'
+>>> str(Git().paginate())
+'git -p'
+>>> str(Git().help())
+'git --help'
+>>> str(Git().paginate().help())
+'git -p --help'
+>>> str(Git().paginate().help().commit().message('bar'))
+'git -p --help commit -m bar'
+
+```
+
+## How to extend the framework. Use Case: git like
+
+### Step 1: inherit from Command class
+
+The command name must be overwritten as class attribute.
+ 
+```python
+from pytocli import Command
+
+
+class Git(Command):
+    name = 'git'
+```
+
+## Step 2: create command option with aux methods
+ 
+```python
+from pytocli import Command
+
+
+class Git(Command):
+    name = 'git'
+
+    def paginate(self):
+        """Add paginate option to git command
+
+        :return: Command for chaining calls
+        """
+        return self.dashed_option('p')
+
+    def help(self):
+        """Add help option to git command
+
+        :return: Command for chaining calls
+        """
+        return self.double_dashed_option('help')
+
+    def start(self, value):
+        return self.dashed_value_option('C', value)
+
+    def exec(self, value):
+        return self.double_dashed_value_option('exec-path', value)
+
+    def fake(self, *values):
+        return self.dashed_multi_value_option('fake', *values)
+
+    def dfake(self, *values):
+        return self.double_dashed_multi_value_option('dfake', *values)
+
+```
+## Step 3: create a subcomman following steps 1 and 2
+ 
+```python
+from pytocli import SubCommand
+
+
+class Commit(SubCommand):
+    name = 'commit'
+
+    def message(self, msg):
+        return self.dashed_value_option('m', msg)
+```
+
+## Step 4: create method on Command connecting it with SubCommand
+ 
+```python
+class Git(Command):
+    name = 'git'
+
+    #other methods are hidden
+    
+    def commit(self):
+        return Commit(self)
 ```
---
+++ tests/test_pytocli.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import pytest
+
+from pytocli import Command, SubCommand
+
+
+class Git(Command):
+    name = 'git'
+
+    def paginate(self):
+        """Add paginate option to git command
+
+        :return: Command for chaining calls
+        """
+        return self.dashed_option('p')
+
+    def help(self):
+        """Add help option to git command
+
+        :return: Command for chaining calls
+        """
+        return self.double_dashed_option('help')
+
+    def start(self, value):
+        return self.dashed_value_option('C', value)
+
+    def exec(self, value):
+        return self.double_dashed_value_option('exec-path', value)
+
+    def fake(self, *values):
+        return self.dashed_multi_value_option('fake', *values)
+
+    def dfake(self, *values):
+        return self.double_dashed_multi_value_option('dfake', *values)
+
+    def commit(self):
+        return Commit(self)
+
+
+class Commit(SubCommand):
+    name = 'commit'
+
+    def message(self, msg):
+        return self.dashed_value_option('m', msg)
+
+
+def test_simple_command():
+    assert 'git' == str(Git())
+
+
+def test_dashed_option():
+    assert 'git -p' == str(Git().dashed_option('p'))
+    assert 'git -p' == str(Git().paginate())
+
+
+def test_dashed_option_duplicated():
+    cmd = Git().paginate()
+    with pytest.raises(ValueError):
+        cmd.paginate()
+
+
+def test_double_dashed_option():
+    assert 'git --help' == str(Git().double_dashed_option('help'))
+
+
+def test_double_dashed_option_duplicated():
+    cmd = Git().double_dashed_option('help')
+    with pytest.raises(ValueError):
+        cmd.double_dashed_option('help')
+
+
+def test_dashed_value_option():
+    assert 'git -C foo.git' == str(Git().dashed_value_option('C', 'foo.git'))
+    assert 'git -C foo.git' == str(Git().start('foo.git'))
+
+
+def test_dashed_value_option_duplicated():
+    cmd = Git().start('foo.git')
+    with pytest.raises(ValueError):
+        cmd.start('bar.git')
+
+
+def test_double_dashed_value_option():
+    assert 'git --exec-path /usr/bin/git' == str(Git().double_dashed_value_option('exec-path', '/usr/bin/git'))
+    assert 'git --exec-path /usr/bin/git' == str(Git().exec('/usr/bin/git'))
+
+
+def test_double_dashed_value_option_duplicated():
+    cmd = Git().exec('foo')
+    with pytest.raises(ValueError):
+        cmd.exec('bar')
+
+
+def test_dashed_with_multiple_values():
+    assert 'git -fake 1 2 3' == str(Git().fake(1, 2, 3))
+    cmd = Git().fake(1)
+    assert 'git -fake 1' == str(cmd)
+    assert 'git -fake 1 2' == str(cmd.fake(2))
+    assert 'git -fake 1 2 3' == str(cmd.fake(3))
+
+
+def test_dashed_missing_value():
+    with pytest.raises(ValueError):
+        Git().fake()
+
+
+def test_double_dashed_with_multiple_values():
+    assert 'git --dfake 1 2 3' == str(Git().dfake(1, 2, 3))
+    cmd = Git().dfake(1)
+    assert 'git --dfake 1' == str(cmd)
+    assert 'git --dfake 1 2' == str(cmd.dfake(2))
+    assert 'git --dfake 1 2 3' == str(cmd.dfake(3))
+
+
+def test_double_dashed_missing_value():
+    with pytest.raises(ValueError):
+        Git().dfake()
+
+
+def test_subcommand():
+    assert 'git commit' == str(Git().commit())
+
+
+def test_complete_chaining():
+    cmd = Git()
+    assert 'git' == str(cmd)
+    assert 'git -p' == str(cmd.paginate())
+    assert 'git -p --exec-path foo' == str(cmd.exec('foo'))
+    assert 'git -p --exec-path foo commit -m bar' == str(cmd.commit().message('bar'))
+    assert 'git -p commit -m baz' == str(Git().paginate().commit().message('baz'))
