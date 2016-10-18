bb99dce262ec8b4ef23ddb15c33ba0e937356190
renzon <renzon@gmail.com>
2016-10-18 09:45:13 -0200

    fixed pep8

--- pytocli/__init__.py
+++ pytocli/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 # Classes to be exposed
 from pytocli._base import (Option,  # noqa
